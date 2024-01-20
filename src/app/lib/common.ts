ef6a83b11123a9e179c3efae170d5eb7d5b91b26
Dinesh Kumar A <dineshkumargits@gmail.com>
2024-01-20 15:47:57 +0530

    [Dinesh] adds randomString util

--- src/app/lib/common.ts
+++ src/app/lib/common.ts
@@ -34,3 +34,16 @@ export const getProfileName = (name: string) => {
     nameArr?.[1]?.charAt(0)?.toUpperCase() || ""
   }`;
 };
+
+export const randomString = (length: number) => {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+};
