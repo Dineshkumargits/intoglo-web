7831d105f35687dbf65e5cccad12b92b8abd4751
Dinesh Kumar A <dineshkumargits@gmail.com>
2024-01-20 13:43:16 +0530

    [Dinesh] adds Docs page component for docs page

---
+++ src/app/components/Docs/index.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import { get } from "@/app/api/api";
+import { Documents } from "@/app/types/documents";
+import { API_ROUTES } from "@/app/utils/constants";
+import { Add, Edit } from "@mui/icons-material";
+import { Card, IconButton, List, ListItem, Stack } from "@mui/joy";
+import Typography from "@mui/joy/Typography";
+import { useEffect, useState } from "react";
+import { UIButton } from "../UIButton";
+import { CreateDocBoxModal } from "./CreateDocBoxModal";
+import ComponentWithLoader from "../ComponentWithLoader";
+
+export function DocsComponent() {
+  const [docs, setDocs] = useState<Documents[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [createModelOpen, setCreateModelOpen] = useState(false);
+  useEffect(() => {
+    fetchDocs();
+  }, []);
+  const fetchDocs = async (currentParentId = 0) => {
+    get({ url: `${API_ROUTES.LIST_DOCS}/${currentParentId}` })
+      .then((response) => {
+        if (response?.data?.data?.documents) {
+          setDocs(response?.data?.data?.documents);
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return (
+    <Stack spacing={2} sx={{}}>
+      <Stack direction={"row"} justifyContent={"space-between"}>
+        <Typography level="h3">Documents</Typography>
+        <UIButton
+          startDecorator={<Add />}
+          onClick={() => {
+            setCreateModelOpen(true);
+          }}
+        >
+          Create DocBox
+        </UIButton>
+      </Stack>
+      <ComponentWithLoader loading={loading}>
+        <List
+          sx={{
+            border: "sm",
+          }}
+        >
+          {docs?.map((doc) => {
+            return <DocItem doc={doc} onClick={() => {}} />;
+          })}
+        </List>
+      </ComponentWithLoader>
+      {docs?.length == 0 && (
+        <Stack alignItems={"center"}>
+          <Typography>No DocBox Found</Typography>
+        </Stack>
+      )}
+      <CreateDocBoxModal
+        open={createModelOpen}
+        onClose={() => {
+          setCreateModelOpen(false);
+        }}
+        documents={docs}
+        onCreate={() => {
+          fetchDocs();
+        }}
+      />
+    </Stack>
+  );
+}
+
+const DocItem = ({ doc, onClick }: { doc: Documents; onClick: () => void }) => {
+  return (
+    <ListItem sx={{}} onClick={onClick}>
+      <Card
+        sx={{
+          "&:hover": { boxShadow: "md" },
+          borderRadius: "sm",
+          width: "100%",
+          cursor: "pointer",
+        }}
+      >
+        <Stack direction={"row"} justifyContent={"space-between"}>
+          <Typography>{doc?.name}</Typography>
+          <IconButton
+            size="sm"
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+          >
+            <Edit />
+          </IconButton>
+        </Stack>
+      </Card>
+    </ListItem>
+  );
+};
